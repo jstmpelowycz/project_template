cb9bad2b8325679fa7b8100f5f50c05231c305d1
jstmpelowycz <dmytro.pelovych@gmail.com>
2024-03-10 20:04:46 +0200

    Add empty functions (IO)

---
+++ app/io/input.py
@@ -0,0 +1,8 @@
+def read_text_from_console():
+    pass
+
+def read_from_file_builtin():
+    pass
+
+def read_from_file_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,5 @@
+def print_text_to_console():
+    pass
+
+def write_to_file_builtin():
+    pass
